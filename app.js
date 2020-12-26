98f950bb37f6db93db998d21f4b0222a6f68f2e0
Abdullah Ansari <aa9272137@gmail.com>
2020-12-26 16:07:27 +0500

    get working post not working

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ let express = require("express"); //use to create server
 let app = express();
 let fs = require("fs"); //use to read file
 let multer = require("multer"); //use to upload files
-let {TesseractWorker} = require("tesseract.js"); //use to read images
-let worker = new TesseractWorker();
+let { createWorker } = require("tesseract.js"); //use to read images
+const worker = createWorker({
+    logger: m => console.log(m),
+    });
 
 //storage setup
 let storage = multer.diskStorage({
-    destination: (req, res, cb) => {
+    destination: (req, file, cb) => {
         cb(null, "./uploads");
     },
     filename: (req, file, cb) => {
@@ -17,4 +19,32 @@ let storage = multer.diskStorage({
 })
 let upload = multer({storage: storage}).single("abdullah"); //here setting up upload
 
+//setting up front end
 app.set("view engine", "ejs");
+
+//server start up
+let PORT = 3000 // if we run this live we have to use environment var
+app.listen(PORT, console.log("Server is running successfully"));
+
+//routes
+app.get("/", (req, res, next) => {
+    res.render("index");
+})
+app.post("/upload", (req,res,next) => {
+    upload(req, res, err => {
+        fs.readFile(`/uploads/${req.file.originalname}`, (err, data) => {
+            if (err){
+                console.log("There is an error in your request", err);
+            }
+            worker
+            .recognize(data, "eng", {tessjs_create_pdf: '1'})
+            .then((result) => {
+                res.send(result.text);
+            })
+            .finally(() => {
+                worker.terminate();
+            })
+        })
+    })
+})
+
