410ba3c823be357c5c6be9b76cf0c99243d7530c
Abdullah Ansari <aa9272137@gmail.com>
2020-12-26 15:11:31 +0500

    added views

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+//dependencies
 let express = require("express"); //use to create server
 let app = express();
 let fs = require("fs"); //use to read file
 let multer = require("multer"); //use to upload files
 let {TesseractWorker} = require("tesseract.js"); //use to read images
 let worker = new TesseractWorker();
+
+//storage setup
+let storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+let upload = multer({storage: storage}).single("abdullah"); //here setting up upload
+
+app.set("view engine", "ejs");
