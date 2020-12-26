b8dcc929de3d0fa192d25c65718197214c232e69
Abdullah Ansari <aa9272137@gmail.com>
2020-12-26 17:37:52 +0500

    AI project done

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ let app = express();
 let fs = require("fs"); //use to read file
 let multer = require("multer"); //use to upload files
 let { createWorker } = require("tesseract.js"); //use to read images
-const worker = createWorker({
-    logger: m => console.log(m),
-    });
 
 //storage setup
 let storage = multer.diskStorage({
@@ -23,21 +20,46 @@ let upload = multer({storage: storage}).single("abdullah"); //here setting up up
 app.set("view engine", "ejs");
 
 //server start up
-let PORT = 3000 // if we run this live we have to use environment var
+let PORT = 30 // if we run this live we have to use environment var
 app.listen(PORT, console.log("Server is running successfully"));
 
 //routes
 app.get("/", (req, res, next) => {
     res.render("index");
 })
-app.post("/upload", (req,res,next) => {
+app.post("/upload", (req, res) => {
+    upload(req, res, (err) => {
+        (async () => {
+            const worker = createWorker({
+                logger: m => console.log(m)
+            });
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            const { data: { text } } = await worker.recognize(`./uploads/${req.file.originalname}`);
+            // console.log(text);
+            const { data } = await worker.getPDF('OCR Result');
+            fs.writeFileSync('ocr-result.pdf', Buffer.from(data));
+            console.log('PDF generated: ocr-result.pdf');
+            res.redirect("/download");
+            await worker.terminate();
+        })();
+    });
+});
+app.get("/download", (req, res) => {
+    const file = `${__dirname}/ocr-result.pdf`;
+    res.download(file);
+});
+/*app.post("/upload", (req,res,next) => {
     upload(req, res, err => {
         fs.readFile(`/uploads/${req.file.originalname}`, (err, data) => {
-            if (err){
-                console.log("There is an error in your request", err);
-            }
+            if(err) return console.log("There is an error in your request", err);
+
+
             worker
             .recognize(data, "eng", {tessjs_create_pdf: '1'})
+            .progress(progress => {
+                console.log(progress)})
             .then((result) => {
                 res.send(result.text);
             })
@@ -47,4 +69,4 @@ app.post("/upload", (req,res,next) => {
         })
     })
 })
-
+*/
