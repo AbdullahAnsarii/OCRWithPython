92dd028836e96343edaf35e7664fc573a3598f8f
Abdullah Ansari <aa9272137@gmail.com>
2021-01-14 17:05:56 +0500

    nothing changed just lines order

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //dependencies
 let express = require("express"); //use to create server
-let app = express();
 let fs = require("fs"); //use to read file
 let multer = require("multer"); //use to upload files
 let { createWorker } = require("tesseract.js"); //use to read images
-
+let app = express();
 //storage setup
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -50,23 +49,3 @@ app.get("/download", (req, res) => {
     const file = `${__dirname}/ocr-result.pdf`;
     res.sendFile(file);
 });
-/*app.post("/upload", (req,res,next) => {
-    upload(req, res, err => {
-        fs.readFile(`/uploads/${req.file.originalname}`, (err, data) => {
-            if(err) return console.log("There is an error in your request", err);
-
-
-            worker
-            .recognize(data, "eng", {tessjs_create_pdf: '1'})
-            .progress(progress => {
-                console.log(progress)})
-            .then((result) => {
-                res.send(result.text);
-            })
-            .finally(() => {
-                worker.terminate();
-            })
-        })
-    })
-})
-*/
